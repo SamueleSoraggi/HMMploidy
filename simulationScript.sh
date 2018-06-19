a5c5bebefd3aa3570eddbcefd2374ef879617e3d
samuele soraggi <samuele@randy>
2018-06-19 11:08:28 +0200

    output with absolute paths, parsed scripts path

--- simulationScript.sh
+++ simulationScript.sh
@@ -7,22 +7,9 @@ do
     key="$1"
     minDp=0
     freqs=(0 0 0)
-    minInd=1
-    FOLDER=.
-    SCRIPTFOLDER=.
-    BASENAME=out
+    BASENAME=./out
     
     case $key in
-	-s|--simulatorFolder)
-	    SCRIPTFOLDER="$2"
-	    shift # past argument
-	    shift # past value
-	    ;;
-	-f|--folder)
-	    FOLDER="$2"
-	    shift # past argument
-	    shift # past value
-	    ;;
 	-o|--out)
 	    BASENAME="$2"
 	    shift # past argument
@@ -30,7 +17,7 @@ do
 	    ;;
 	-p|--ploidy)
 	    ploidy1="$2"
-	    IFS=',' read -ra ploidy <<< "$ploidy1"	
+	    IFS=',' read -ra ploidy <<< "$ploidy1" #remove string ','	
 	    shift # past argument
 	    shift # past value
 	    ;;
@@ -51,24 +38,29 @@ do
 	    shift # past argument
 	    shift # past value
 	    ;;
-	-g|--minGlobalDepth)
-	    minDp="$2"
-	    #IFS=',' read -ra minDp <<< "$minDp1"
+	-v|--verbose)
+	    VERBOSE=TRUE
 	    shift # past argument
 	    shift # past value
 	    ;;
-	-m|--minInd)
-	    minInd="$2"
-	    #IFS=',' read -ra minInd <<< "$minInd1"
-	    shift # past argument
-	    shift # past value
-	    ;;
-	-q|--minorFreq)
-	    freqs1="$2"
-	    IFS=',' read -ra freqs <<< "$freqs1"
-	    shift # past argument
-	    shift # past value
-	    ;;
+	#-g|--minGlobalDepth)
+	#    minDp="$2"
+	#    #IFS=',' read -ra minDp <<< "$minDp1"
+	#    shift # past argument
+	#    shift # past value
+	#    ;;
+	#-m|--minInd)
+	#    minInd="$2"
+	#    #IFS=',' read -ra minInd <<< "$minInd1"
+	#    shift # past argument
+	#    shift # past value
+	#    ;;
+	#-q|--minorFreq)
+	#    freqs1="$2"
+	#    IFS=',' read -ra freqs <<< "$freqs1"
+	#    shift # past argument
+	#    shift # past value
+	#    ;;
 	--default)
 	    DEFAULT=YES
 	    shift # past argument
@@ -81,18 +73,26 @@ do
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-FILELIST=names.${BASENAME}.filelist
+#reformat to assign absolute path, so that the list of files
+#can be used from anywhere 
+BBB=`realpath $BASENAME`
+BASENAME=`dirname $BBB`/`basename $BASENAME`
+
+FILELIST=${BASENAME}.filelist
 rm -f $FILELIST
+
+#FIND SOURCE FOLDER OF THE SCRIPTS
+SCRIPTFOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #generate data for all combinations of parameters (sample and depth)
 for SAM in ${samples[@]}
 do
     for DP in ${depth[@]}
     do
-	NAME=$FOLDER/$BASENAME.DP${DP}.NIND${SAM} #basename for the file
-	echo "GENERATING FILE: " $NAME
+	NAME=$BASENAME.DP${DP}.NIND${SAM} #basename for the file
+	echo "GENERATING FILE: " "\"" $NAME "\""
 	offset=1 #loci counter
-	rm -f $NAME.mpileup.gz $NAME.txt $NAME.fai
+	rm -f $NAME.mpileup.gz $NAME.txt
 	printf 'Chrom\tStart\tend\n'
 	
 	for PL in ${ploidy[@]} #sequentially generate ploidy levels
@@ -103,7 +103,8 @@ do
 	    Rscript ${SCRIPTFOLDER}/simulMpileup.R --out test.DP${DP}.NIND${SAM}.txt --copy ${PL}x${SAM} --sites $sites --depth $A --qual 20 --ksfs 1 --ne 10000 --offset $offset | gzip -c -f > $NAME.BUFFER.mpileup.gz
 
 	    printf 'copy_%dx%d\t%d\t%d\n' "$PL" "$SAM" "$offset" "$(($offset + $sites - 1))"
-	    printf 'copy_%dx%d\t%d\t%d\n' "$PL" "$SAM" "$offset" "$(($offset + $sites - 1))" >> $NAME.fai
+
+	    #printf 'copy_%dx%d\t%d\t%d\n' "$PL" "$SAM" "$offset" "$(($offset + $sites - 1))" >> $NAME.fai
 
 	    #keep track of number of loci
 	    offset=$(($offset + $sites))
@@ -122,6 +123,4 @@ do
     done
 done
 
-echo "End of simulations :)"
-
-#python3 ${SCRIPTFOLDER}/Genotype_Likelihoods.py ${FILEFOLDER} -dp $minDp -m ${freqs[0]} -M2 ${freqs[1]} -M3 ${freqs[2]} ${FILEFOLDER}
+echo "output file list " "\"" $FILELIST "\""
